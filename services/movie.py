1aeb9b14581e1799b83cb01c0b3bf71a11cd39e5
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2024-10-27 21:07:34 +0200

    Solution

--- services/movie.py
+++ services/movie.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from django.db.models import QuerySet
 
 from db.models import Movie
@@ -6,7 +7,8 @@ from db.models import Movie
 def get_movies(
     genres_ids: list[int] = None,
     actors_ids: list[int] = None,
-) -> QuerySet:
+    title: str = None,
+) -> QuerySet[Movie]:
     queryset = Movie.objects.all()
 
     if genres_ids:
@@ -15,26 +17,34 @@ def get_movies(
     if actors_ids:
         queryset = queryset.filter(actors__id__in=actors_ids)
 
+    if title:
+        queryset = queryset.filter(title__icontains=title)
+
     return queryset
 
 
 def get_movie_by_id(movie_id: int) -> Movie:
-    return Movie.objects.get(id=movie_id)
+    try:
+        return Movie.objects.get(id=movie_id)
+    except Movie.DoesNotExist:
+        raise ValueError(f"Movie with id {movie_id} does not exist.")
 
 
+@transaction.atomic
 def create_movie(
     movie_title: str,
     movie_description: str,
-    genres_ids: list = None,
-    actors_ids: list = None,
+    genres_ids: list[int] = None,
+    actors_ids: list[int] = None
 ) -> Movie:
-    movie = Movie.objects.create(
+    new_movie = Movie.objects.create(
         title=movie_title,
         description=movie_description,
     )
-    if genres_ids:
-        movie.genres.set(genres_ids)
-    if actors_ids:
-        movie.actors.set(actors_ids)
 
-    return movie
+    if genres_ids:
+        new_movie.genres.set(genres_ids)
+    if actors_ids:
+        new_movie.actors.set(actors_ids)
+
+    return new_movie
--- services/movie_session.py
+++ services/movie_session.py
@@ -4,7 +4,9 @@ from db.models import MovieSession
 
 
 def create_movie_session(
-    movie_show_time: str, movie_id: int, cinema_hall_id: int
+    movie_show_time: str,
+    movie_id: int,
+    cinema_hall_id: int
 ) -> MovieSession:
     return MovieSession.objects.create(
         show_time=movie_show_time,
@@ -13,7 +15,7 @@ def create_movie_session(
     )
 
 
-def get_movies_sessions(session_date: str = None) -> QuerySet:
+def get_movies_sessions(session_date: str = None) -> QuerySet[MovieSession]:
     queryset = MovieSession.objects.all()
     if session_date:
         queryset = queryset.filter(show_time__date=session_date)
@@ -21,7 +23,11 @@ def get_movies_sessions(session_date: str = None) -> QuerySet:
 
 
 def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
-    return MovieSession.objects.get(id=movie_session_id)
+    try:
+        return MovieSession.objects.get(id=movie_session_id)
+    except MovieSession.DoesNotExist:
+        raise ValueError("Movie session with id "
+                         f"{movie_session_id} does not exist.")
 
 
 def update_movie_session(
@@ -30,15 +36,23 @@ def update_movie_session(
     movie_id: int = None,
     cinema_hall_id: int = None,
 ) -> None:
-    movie_session = MovieSession.objects.get(id=session_id)
+    movie_session = get_movie_session_by_id(session_id)
+
     if show_time:
         movie_session.show_time = show_time
     if movie_id:
         movie_session.movie_id = movie_id
     if cinema_hall_id:
         movie_session.cinema_hall_id = cinema_hall_id
+
     movie_session.save()
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
     MovieSession.objects.get(id=session_id).delete()
+
+
+def get_taken_seats(movie_session_id: int) -> list[dict[str, int]]:
+    movie_session = get_movie_session_by_id(movie_session_id)
+    tickets = movie_session.tickets.all()
+    return [{"row": ticket.row, "seat": ticket.seat} for ticket in tickets]
