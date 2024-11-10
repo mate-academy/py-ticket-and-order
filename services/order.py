9fd5fbdc85214ffdc7f0511431b4c5167145f96f
Den_k0 <denys.kotovskiy@gmail.com>
2024-11-10 14:13:47 +0200

    Solution fix

--- services/order.py
+++ services/order.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+from typing import Optional
+
 from django.contrib.auth import get_user_model
 
 from django.db import transaction
@@ -13,7 +15,7 @@ from db.models import Order, Ticket
 def create_order(
         tickets: list[dict],
         username: str,
-        date: datetime = None,
+        date: Optional[datetime] = None,
 ) -> Order:
     user = get_user_model().objects.get(username=username)
     order = Order.objects.create(
@@ -35,7 +37,7 @@ def create_order(
     return order
 
 
-def get_orders(username: str = None) -> QuerySet[Order]:
+def get_orders(username: Optional[str] = None) -> QuerySet[Order]:
     if username:
         user = get_user_model().objects.get(username=username)
         return Order.objects.filter(user=user)
