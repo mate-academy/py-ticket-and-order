4dfd040131c545e76b7e29a61a83ef7468787afb
Andrey Dementyev <dementyev.andreyy@gmail.com>
2024-11-10 16:21:23 +0200

    'Solution'

--- services/movie.py
+++ services/movie.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from django.db.models import QuerySet
 
 from db.models import Movie
@@ -6,7 +7,8 @@ from db.models import Movie
 def get_movies(
     genres_ids: list[int] = None,
     actors_ids: list[int] = None,
-) -> QuerySet:
+    title: str = None
+) -> QuerySet[Movie]:
     queryset = Movie.objects.all()
 
     if genres_ids:
@@ -15,6 +17,9 @@ def get_movies(
     if actors_ids:
         queryset = queryset.filter(actors__id__in=actors_ids)
 
+    if title:
+        queryset = queryset.filter(title__icontains=title)
+
     return queryset
 
 
@@ -22,19 +27,20 @@ def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(id=movie_id)
 
 
+@transaction.atomic
 def create_movie(
     movie_title: str,
     movie_description: str,
-    genres_ids: list = None,
-    actors_ids: list = None,
+    genres_ids: list[int] = None,
+    actors_ids: list[int] = None,
 ) -> Movie:
-    movie = Movie.objects.create(
+    new_movie = Movie.objects.create(
         title=movie_title,
         description=movie_description,
     )
     if genres_ids:
-        movie.genres.set(genres_ids)
+        new_movie.genres.set(genres_ids)
     if actors_ids:
-        movie.actors.set(actors_ids)
+        new_movie.actors.set(actors_ids)
 
-    return movie
+    return new_movie
