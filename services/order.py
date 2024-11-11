618df39d62b9fad1e9ed5dbb5f000f5632a015c7
Matvii Safonov <matveysafonov2@gmail.com>
2024-11-11 16:51:10 +0200

    Solution

--- services/order.py
+++ services/order.py
@@ -1,11 +1,7 @@
 from datetime import datetime
-
 from django.contrib.auth import get_user_model
-
 from django.db import transaction
-
 from django.db.models import QuerySet
-
 from db.models import Order, Ticket
 
 
