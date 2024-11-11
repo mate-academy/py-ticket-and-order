e344e84a2d7eff9e64f6ed5fd6538405083def5f
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2024-11-11 10:12:28 +0200

    Fixed flake8

--- services/order.py
+++ services/order.py
@@ -7,7 +7,11 @@ from db.models import Ticket, Order, User
 
 
 @transaction.atomic
-def create_order(tickets: list[dict], username: str, date: str = None) -> Order:
+def create_order(
+        tickets: list[dict],
+        username: str,
+        date: str = None
+) -> Order:
     try:
         user = get_user_model().objects.get(username=username)
     except User.DoesNotExist:
