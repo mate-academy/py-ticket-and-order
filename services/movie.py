9837f3595c7fbea09b51ee132cfbc4d702d5f894
Oleksandr <rigby8806@gmail.com>
2024-12-14 18:56:02 +0200

    Solution

--- services/movie.py
+++ services/movie.py
@@ -3,10 +3,11 @@ from django.db.models import QuerySet
 
 from db.models import Movie
 
+
 def get_movies(
-    genres_ids: list[int] = None,
-    actors_ids: list[int] = None,
-    title: str = None,
+        genres_ids: list[int] = None,
+        actors_ids: list[int] = None,
+        title: str = None,
 ) -> QuerySet[Movie]:
     queryset = Movie.objects.all()
 
@@ -19,6 +20,7 @@ def get_movies(
         queryset = queryset.filter(title__icontains=title)
 
     return queryset
+
 
 def get_movie_by_id(movie_id: int) -> Movie:
         return Movie.objects.get(id=movie_id)
