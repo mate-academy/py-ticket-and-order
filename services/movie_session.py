63ae2b97421831fa9536bf5d35b5e1bb8be7d284
Oleksandr <rigby8806@gmail.com>
2024-12-14 19:00:12 +0200

    Solution

--- services/movie_session.py
+++ services/movie_session.py
@@ -16,3 +16,8 @@ def get_movies_sessions(session_date: str = None) -> QuerySet[MovieSession]:
     queryset = MovieSession.objects.all()
     if session_date:
         queryset = queryset.filter(show_time__date=session_date)
+
+
+def get_taken_seats(movie_session_id: int) -> list[dict]:
+    tickets = MovieSession.objects.get(id=movie_session_id).tickets.all()
+    return [{"row": ticket.row, "seat": ticket.seat} for ticket in tickets]
