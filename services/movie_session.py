a111e2d554d52a046076dbd40a0dfc78db1dc2fa
Tymofii Pavliohlo <tim13fiz@gmail.com>
2024-12-15 11:50:50 +0100

    Solution

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,8 +1,5 @@
 from django.db.models import QuerySet
-
 from db.models import MovieSession
-
-
 def create_movie_session(
     movie_show_time: str, movie_id: int, cinema_hall_id: int
 ) -> MovieSession:
@@ -13,16 +10,14 @@ def create_movie_session(
     )
 
 
-def get_movies_sessions(session_date: str = None) -> QuerySet:
+def get_movies_sessions(session_date: str = None) -> QuerySet[MovieSession]:
     queryset = MovieSession.objects.all()
     if session_date:
         queryset = queryset.filter(show_time__date=session_date)
     return queryset
 
-
 def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
     return MovieSession.objects.get(id=movie_session_id)
-
 
 def update_movie_session(
     session_id: int,
@@ -39,17 +34,10 @@ def update_movie_session(
         movie_session.cinema_hall_id = cinema_hall_id
     movie_session.save()
 
-
 def delete_movie_session_by_id(session_id: int) -> None:
     MovieSession.objects.get(id=session_id).delete()
 
 
-def get_taken_seats(movie_session_id: int) -> list[dict[str, int]]:
-    movie_session = MovieSession.objects.prefetch_related(
-        "tickets"
-    ).get(id=movie_session_id)
-    taken_seats = [
-        {"row": ticket.row, "seat": ticket.seat}
-        for ticket in movie_session.tickets.all()
-    ]
-    return taken_seats
+def get_taken_seats(movie_session_id: int) -> list[dict]:
+    tickets = MovieSession.objects.get(id=movie_session_id).tickets.all()
+    return [{"row": ticket.row, "seat": ticket.seat} for ticket in tickets]
--- services/order.py
+++ services/order.py
@@ -1,61 +1,39 @@
-from typing import Any
 from datetime import datetime
-
-from django.core.exceptions import ValidationError
+from django.contrib.auth import get_user_model
 from django.db import transaction
 from django.db.models import QuerySet
-
-from db.models import Order, User, MovieSession, Ticket
-
-
-def get_orders(
-        username: str = None
-) -> QuerySet:
-    queryset = Order.objects.all().order_by("-id")
-    if username:
-        queryset = queryset.filter(user__username=username).order_by("-id")
-
-    return queryset
+from db.models import Ticket, Order, MovieSession
 
 
+@transaction.atomic
 def create_order(
-    tickets: list[dict[str, Any]],
+    tickets: list[dict],
     username: str,
-    date: str = None
-) -> Order:
-    try:
-        user = User.objects.get(username=username)  # Retrieve the user
-    except User.DoesNotExist:
-        raise ValidationError(f"User with username"
-                              f" '{username}' does not exist.")
+    date: datetime = None,
+) -> None:
 
-    with transaction.atomic():
-        # Create the order
-        order = Order.objects.create(user=user)
+    order = Order.objects.create(
+        user=get_user_model().objects.get(username=username)
+    )
+
+    for ticket in tickets:
         if date:
-            created_at = datetime.strptime(date, "%Y-%m-%d %H:%M")
-            order.created_at = created_at
-        order.save()
+            order.created_at = date
+            order.save()
 
-        for ticket_data in tickets:
-            required_fields = {"row", "seat", "movie_session"}
-            if not required_fields.issubset(ticket_data):
-                raise ValidationError("Missing required ticket fields")
+    movie_session = MovieSession.objects.get(id=ticket["movie_session"])
+    Ticket.objects.create(
+        movie_session=movie_session,
+        order=order,
+        row=ticket["row"],
+        seat=ticket["seat"],
+    )
 
-            try:
-                movie_session = MovieSession.objects.get(
-                    id=ticket_data["movie_session"]
-                )
-            except MovieSession.DoesNotExist:
-                raise ValidationError(
-                    f"Movie session with ID"
-                    f" {ticket_data["movie_session"]}"
-                    f" does not exist.")
 
-            Ticket.objects.create(
-                row=ticket_data["row"],
-                seat=ticket_data["seat"],
-                movie_session=movie_session,
-                order=order
-            )
-    return order
+def get_orders(username: str = None) -> QuerySet[Order]:
+    if username:
+        return Order.objects.filter(
+            user=get_user_model().objects.get(username=username)
+        )
+
+    return Order.objects.all()
