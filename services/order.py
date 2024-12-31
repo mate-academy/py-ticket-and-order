2abb293878d0eab8d4c4126f0cc37b16e60f060c
Dmytro <labtech.kh@gmail.com>
2024-12-31 20:30:00 +0200

    'Solution_v04'

--- services/order.py
+++ services/order.py
@@ -7,32 +7,32 @@ from django.db.models import QuerySet
 from db.models import Ticket, Order, MovieSession
 
 
+@transaction.atomic
 def create_order(tickets: list,
                  username: str,
                  date: datetime = None) -> list[Ticket]:
-    with transaction.atomic():
-        ticket_objects = []
-        user, user_created = get_user_model(
-        ).objects.get_or_create(username=username)
-        order, order_created = Order.objects.get_or_create(user=user)
-        if date:
-            order.created_at = date
-            order.save()
-        for ticket in tickets:
-            row = ticket["row"]
-            seat = ticket["seat"]
-            movie_session = MovieSession.objects.get(
-                id=ticket["movie_session"])
-            ticket_objects.append(Ticket(
-                movie_session=movie_session,
-                order=order,
-                row=row,
-                seat=seat
-            ))
-        return Ticket.objects.bulk_create(ticket_objects)
+    ticket_objects = []
+    user, user_created = get_user_model(
+    ).objects.get_or_create(username=username)
+    order, order_created = Order.objects.get_or_create(user=user)
+    if date:
+        order.created_at = date
+        order.save()
+    for ticket in tickets:
+        row = ticket["row"]
+        seat = ticket["seat"]
+        movie_session = MovieSession.objects.get(
+            id=ticket["movie_session"])
+        ticket_objects.append(Ticket(
+            movie_session=movie_session,
+            order=order,
+            row=row,
+            seat=seat
+        ))
+    return Ticket.objects.bulk_create(ticket_objects)
 
 
-def get_orders(username: str = None) -> QuerySet:
+def get_orders(username: str = None) -> QuerySet[Order]:
     if username:
         return Order.objects.filter(user__username=username)
     return Order.objects.all().order_by("-user")
