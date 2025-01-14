ff03e44578ece84a8de73cf5694e526537f39cdb
Nazarii Khalimonov <nazar131188@gmail.com>
2025-01-14 12:00:49 +0200

    'solution'

--- db/models.py
+++ db/models.py
@@ -1,8 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.core.exceptions import ValidationError
-
-import settings
+from django.conf import settings
 
 
 class Genre(models.Model):
