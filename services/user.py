0c58600b2799b2f572790bce20d6bfc1519a9dad
Question <make772@ukr.net>
2025-02-02 01:03:46 +0200

    result

---
+++ services/user.py
@@ -0,0 +1,66 @@
+from typing import Optional
+
+from django.core.exceptions import ObjectDoesNotExist
+from db.models import User
+
+
+def create_user(
+        username: str,
+        password: str,
+        email: str = None,
+        first_name: str = None,
+        last_name: str = None
+) -> User:
+    user = User.objects.create_user(username=username, password=password)
+    if email:
+        user.email = email
+    if first_name:
+        user.first_name = first_name
+    if last_name:
+        user.last_name = last_name
+
+    user.save()
+
+    return user
+
+
+def get_user(user_id: int) -> Optional[User]:
+    try:
+        user = User.objects.get(id=user_id)
+        return user
+    except ObjectDoesNotExist:
+        return None
+
+
+def update_user(
+        user_id: int,
+        username: str = None,
+        password: str = None,
+        email: str = None,
+        first_name: str = None,
+        last_name: str = None
+) -> Optional[User]:
+    try:
+        user = User.objects.get(id=user_id)
+
+        if username:
+            user.username = username
+
+        if email:
+            user.email = email
+
+        if first_name:
+            user.first_name = first_name
+
+        if last_name:
+            user.last_name = last_name
+
+        if password:
+            user.set_password(password)
+
+        user.save()
+
+        return user
+
+    except User.DoesNotExist:
+        return None
