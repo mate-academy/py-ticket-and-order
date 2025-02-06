a2055a8dd685c9507acd38dc316d0f649756dd4d
unknown <maximuschampion2002@gmail.com>
2025-02-06 14:06:26 +0200

    tickets-and-orders

--- services/order.py
+++ services/order.py
@@ -1,5 +1,6 @@
 from typing import List, Optional
 
+from django.core.exceptions import ValidationError
 from django.db import transaction
 from django.db.models import QuerySet
 from django.utils import timezone
@@ -9,15 +10,19 @@ from db.models import User, Order, MovieSession, Ticket
 
 
 @transaction.atomic
-def create_order(tickets: List[dict],
-                 username: str, date: Optional[str] = None) -> None:
-
-    user = User.objects.get(username=username)
+def create_order(tickets: List[dict], username: str, date: Optional[str] = None) -> None:
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        raise ValidationError(f"User with username '{username}' does not exist.")
 
     order = Order.objects.create(user=user)
 
     if date:
-        created_at = datetime.strptime(date, "%Y-%m-%d %H:%M")
+        try:
+            created_at = datetime.strptime(date, "%Y-%m-%d %H:%M")
+        except ValueError:
+            raise ValidationError("Date format is incorrect. Expected format: YYYY-MM-DD HH:MM")
     else:
         created_at = timezone.now()
 
@@ -25,14 +30,18 @@ def create_order(tickets: List[dict],
     order.save()
 
     for ticket_data in tickets:
-        movie_session = MovieSession.objects.get(
-            id=ticket_data["movie_session"])
+        try:
+            movie_session = MovieSession.objects.get(id=ticket_data["movie_session"])
+        except MovieSession.DoesNotExist:
+            raise ValidationError(f"Movie session with ID '{ticket_data['movie_session']}' does not exist.")
+
         Ticket.objects.create(
             movie_session=movie_session,
             order=order,
             row=ticket_data["row"],
             seat=ticket_data["seat"],
         )
+
 
 
 def get_orders(username: Optional[str] = None) -> QuerySet:
