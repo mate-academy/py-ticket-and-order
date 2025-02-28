d6a1ee8dd07818f0dc89b7af2ae2e93d11a6b9eb
PythonToGo <pythontogoplease@gmail.com>
2025-02-28 16:18:49 +0100

    Solution2

--- services/order.py
+++ services/order.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from db.models import Order, User, Ticket, MovieSession
+from db.models import Order, User, Ticket
 from django.db.models import QuerySet
 from django.db import transaction
 
@@ -7,34 +7,23 @@ from django.db import transaction
 def create_order(tickets: list,
                  username: str,
                  date: datetime = None) -> Order:
-
     with transaction.atomic():
         user = User.objects.get(username=username)
         order = Order.objects.create(user=user)
         if date:
             order.created_at = date
-            order.save()
 
-        ticket_ = []
         for ticket in tickets:
-            if not (
-                MovieSession.objects.filter(id=ticket["movie_session"])
-                .exists()
-            ):
-                raise ValueError(
-                    f"Invalid movie_session_id: {ticket["movie_session"]}"
-                )
+            ticket_ = Ticket.objects.create(
+                movie_session_id=ticket["movie_session"],
+                row=ticket["row"],
+                seat=ticket["seat"],
+                order=order)
+            ticket_.save()
 
-            ticket_.append(
-                Ticket(movie_session_id=ticket["movie_session"],
-                       row=ticket["row"],
-                       seat=ticket["seat"],
-                       order=order
-                       )
-            )
+        order.save()
 
-        Ticket.objects.bulk_create(ticket_)
-    return order
+        return order
 
 
 def get_orders(username: str = None) -> QuerySet:
